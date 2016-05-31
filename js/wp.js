97777a0e283b1d141f99bbabbe849b7560d18dea
oiler <jrf1039@gmail.com>
2016-05-31 16:21:35 -0400

    new wp links

--- js/wp.js
+++ js/wp.js
@@ -1,10 +1,17 @@
 { "links" : {
 	"DEV": "",
+	"wpdb": "https://codex.wordpress.org/Class_Reference/wpdb#Show_and_Hide_SQL_Errors",
+	"mysqli": "http://php.net/manual/en/function.mysqli-connect.php",
+
+	"Field Manager": "",
 	"field manager docs": "http://fieldmanager.org/docs/",
 	"field manager api": "http://api.fieldmanager.org/",
 
-	"PRODUCT": "",
-	"": ""
+	"WP CLI": "",
+	"internal api": "https://wp-cli.org/docs/internal-api/",
+	"commands": "http://wp-cli.org/docs/commands-cookbook/",
+	"tools": "http://wp-cli.org/docs/tools/",
+	"packages": "http://wp-cli.org/package-index/",
 	}
 }
 
