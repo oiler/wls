1aa004dd5ee1b91679e3bd9c10158d76ecb511b4
oiler <jrf1039@gmail.com>
2016-06-20 09:21:07 -0400

    wp

--- js/wp.js
+++ js/wp.js
@@ -11,6 +11,10 @@
 	"repos": "",
 	"jaquith skeleton": "https://github.com/markjaquith/WordPress-Skeleton",
 
+	"de.wp": "",
+	"plugins": "https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/",
+	"child": "https://developer.wordpress.org/themes/advanced-topics/child-themes/".
+
 	"WP CLI": "",
 	"internal api": "https://wp-cli.org/docs/internal-api/",
 	"commands": "http://wp-cli.org/docs/commands-cookbook/",
